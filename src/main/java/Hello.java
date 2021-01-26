23f413a60873c2b97d81ca2fbdc28566459041c6
Saltanat2020 <saabdubek@gmail.com>
2021-01-26 15:15:36 -0500

    Learning more git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
 //testing Hello
         System.out.println("Hello World");
+        System.out.println("B21 future SDET learning git");
     }
 
 }
